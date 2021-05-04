35248283bba02c97f996e2c9d9459595c05fa060
window101 <hwajun15@naver.com>
2021-05-05 00:10:55 +0900

    COMMIT

--- loto.js
+++ loto.js
@@ -11,3 +11,15 @@ var 후보군 = Array(45)
     }
     var 보너스 = 셔플[셔플.length-1];
     var 당첨숫자들 = 셔플.slice(0, 6).sort((left, right) => {return left-right;});
+
+    var 결과창 = document.getElementById('결과창');
+
+    for(var i = 0; i<당첨숫자들.length; i+=1) {
+        var 공 = document.createElement('div');
+        공.textContent = 당첨숫자들[i];
+        결과창.appendChild(공);
+    }
+    var 보너스칸 = document.getElementsByClassName('보너스')[0];
+    var 보너스공 = document.createElement('div');
+    보너스공.textContent = 보너스;
+    보너스칸.appendChild(보너스공);
